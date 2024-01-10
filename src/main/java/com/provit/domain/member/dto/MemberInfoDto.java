08a1c4bb2fe9780168d76536553397f47d0cbbf9
one-zeze <tales0708@gmail.com>
2024-01-11 06:51:14 +0900

    내정보조회 return 값 추가

--- src/main/java/com/provit/domain/member/dto/MemberInfoDto.java
+++ src/main/java/com/provit/domain/member/dto/MemberInfoDto.java
@@ -8,11 +8,17 @@ import lombok.Data;
 @Data
 public class MemberInfoDto {
 	private final String username;
+    private final String useraccount;
 	private final String name;
+    private final String password;
+    private final String birth;
 
-	@Builder
+    @Builder
     public MemberInfoDto(Member member) {
-        this.name = member.getName();
         this.username = member.getUsername();
+        this.useraccount = member.getUseraccount();
+        this.name = member.getName();
+        this.password = member.getPassword();
+        this.birth = member.getBirth();
     }
 }
