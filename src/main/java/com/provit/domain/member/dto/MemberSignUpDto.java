b247d3e62c550e8104cb056757a2e34f43f9bec3
one-zeze <tales0708@gmail.com>
2024-01-11 06:54:16 +0900

    Member 필드 추가에 따른, toEntity() 수정

--- src/main/java/com/provit/domain/member/dto/MemberSignUpDto.java
+++ src/main/java/com/provit/domain/member/dto/MemberSignUpDto.java
@@ -2,8 +2,15 @@ package com.provit.domain.member.dto;
 
 import com.provit.domain.member.Member;
 
-public record MemberSignUpDto(String username, String password, String name) {
+public record MemberSignUpDto(String username, String useraccount,  String name, String password, String birth) {
 	public Member toEntity() {
-		return Member.builder().username(username).password(password).name(name).build();
+
+		return Member.builder()
+				.username(username)
+				.useraccount(useraccount)
+				.name(name)
+				.password(password)
+				.birth(birth)
+				.build();
 	}
 }
