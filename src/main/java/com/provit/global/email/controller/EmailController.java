5d71f6a286975ceed72caa53621210065939f59e
one-zeze <tales0708@gmail.com>
2024-01-12 20:46:56 +0900

    이메일 인증로직 수정

--- src/main/java/com/provit/global/email/controller/EmailController.java
+++ src/main/java/com/provit/global/email/controller/EmailController.java
@@ -9,7 +9,10 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Map;
 
 @RestController
 @RequiredArgsConstructor
@@ -18,9 +21,12 @@ public class EmailController {
     private final EmailService emailService;
 
     @PostMapping("/sendEmail")
-    public void sendEmail(HttpServletRequest request) throws MessagingException {
-        log.info(request.getParameter("email"));
-        emailService.test1(request.getParameter("email"));
+    public ResponseEntity<?> sendEmail(@RequestBody Map<String, String> request) throws MessagingException {
+        /*
+        * 요청 들어온 request uri를 받아서, 인라인 태그로 걸어주고 사용하면 될 듯
+        * */
+        log.info(request.get("email"));
+        return emailService.sendEmail(request.get("email"));
     }
 
     @GetMapping("/emailAuth")
--- src/main/java/com/provit/global/email/service/EmailService.java
+++ src/main/java/com/provit/global/email/service/EmailService.java
@@ -1,8 +1,12 @@
 package com.provit.global.email.service;
 
+import com.provit.domain.member.Member;
+import com.provit.domain.member.repository.MemberRepository;
 import jakarta.mail.MessagingException;
 import jakarta.mail.internet.MimeMessage;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.mail.javamail.MimeMessageHelper;
@@ -10,22 +14,45 @@ import org.springframework.stereotype.Service;
 
 import java.security.NoSuchAlgorithmException;
 import java.security.SecureRandom;
+import java.util.Map;
 import java.util.Random;
 
 @Service
 @RequiredArgsConstructor
 public class EmailService {
     private final JavaMailSender javaMailSender;
+    private final MemberRepository memberRepository;
 
-    public void sendEmail(String toEmail, String title, String text) throws MessagingException {
+    public ResponseEntity<Map<String, Object>> sendEmail(String email) throws MessagingException {
+        Member existMeber = memberRepository.findByUseraccount(email).orElse(null);
+        if (existMeber != null){
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST)
+                    .body(Map.of("message", "이미 사용 중인 이메일입니다."));
+        }
+
         MimeMessage mimeMessage = javaMailSender.createMimeMessage();
         MimeMessageHelper helper = new MimeMessageHelper(mimeMessage, true, "UTF-8");
 
-        helper.setTo(toEmail);
+        String code = createCode();
+        String title = "SMTP 테스트";
+        String text = "<div>PROVIT 회원 가입 인증 이메일 입니다.</div>"
+                + "<p>인증번호: " + code + "</p>"
+                + "<a href='http://localhost:8080/emailAuth' target='_blank' style='\n" +
+                "    display: inline-block;\n" +
+                "    padding: 10px 20px;\n" +
+                "    background-color: #ffffff;\n" +
+                "    color: #000000;\n" +
+                "    text-decoration: none;\n" +
+                "    border: 1px solid #000000;\n" +
+                "    border-radius: 5px;\n" +
+                "'>이메일 인증 확인</a>";
+
+        helper.setTo(email);
         helper.setSubject(title);
         helper.setText(text, true);
-
         javaMailSender.send(mimeMessage);
+
+        return ResponseEntity.ok().body(Map.of("message", "전송완료"));
     }
     public SimpleMailMessage createEmailForm(String toEmail, String title, String text) {
         SimpleMailMessage message = new SimpleMailMessage();
@@ -47,25 +74,5 @@ public class EmailService {
         } catch (NoSuchAlgorithmException e) {
             return "MemberService.createCode() exception occur";
         }
-    }
-
-    public void test1(String email) throws MessagingException {
-        String toEmail = email;
-        String code = createCode();
-        String title = "SMTP 테스트";
-        String text = "<div>PROVIT 회원 가입 인증 이메일 입니다.</div>"
-                + "<p>인증번호: " + code + "</p>"
-                + "<a href='http://localhost:8080/emailAuth' target='_blank' style='\n" +
-                "    display: inline-block;\n" +
-                "    padding: 10px 20px;\n" +
-                "    background-color: #ffffff;\n" +
-                "    color: #000000;\n" +
-                "    text-decoration: none;\n" +
-                "    border: 1px solid #000000;\n" +
-                "    border-radius: 5px;\n" +
-                "'>이메일 인증 확인</a>";
-
-
-        sendEmail(toEmail, title, text);
     }
 }
