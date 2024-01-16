884fc4e6b0a9bbc2e67ecd8be55fbd9c6797f7be
one-zeze <tales0708@gmail.com>
2024-01-16 23:31:45 +0900

    Custom Annotation for Schedule Date type field

---
+++ src/main/java/com/provit/domain/schedule/annotation/ScheduleDate.java
@@ -0,0 +1,20 @@
+package com.provit.domain.schedule.annotation;
+
+import com.fasterxml.jackson.annotation.JacksonAnnotationsInside;
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.databind.annotation.JsonSerialize;
+import com.fasterxml.jackson.databind.ser.std.ToStringSerializer;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Target({ElementType.ANNOTATION_TYPE, ElementType.FIELD, ElementType.METHOD, ElementType.PARAMETER})
+@Retention(RetentionPolicy.RUNTIME)
+@JacksonAnnotationsInside
+@JsonSerialize(using = ToStringSerializer.class)
+@JsonInclude(JsonInclude.Include.NON_NULL)
+public @interface ScheduleDate {
+    String pattern() default "yyyy-MM-dd";
+}
