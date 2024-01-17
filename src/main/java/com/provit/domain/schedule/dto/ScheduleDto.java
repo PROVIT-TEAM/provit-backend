2d85b0bef12036cb1a2bc6d315ade10c3e6ac6df
one-zeze <tales0708@gmail.com>
2024-01-17 20:28:18 +0900

    Add @NotBlank annotation

--- src/main/java/com/provit/domain/schedule/dto/ScheduleDto.java
+++ src/main/java/com/provit/domain/schedule/dto/ScheduleDto.java
@@ -1,5 +1,6 @@
 package com.provit.domain.schedule.dto;
 
+import jakarta.validation.constraints.NotBlank;
 import lombok.Builder;
 import lombok.Data;
 
@@ -7,9 +8,13 @@ import lombok.Data;
 @Builder
 public class ScheduleDto {
 
+    private final Long id;
+    @NotBlank
     private final String title;
     private final String content;
+    @NotBlank
     private final String startDate;
+    @NotBlank
     private final String endDate;
 
 }
