d98f716e49b63c9aea2573f7284bc36eb8e1f2ae
one-zeze <tales0708@gmail.com>
2024-01-17 20:27:58 +0900

    Add schedule update endpoint, @Valid annotation

--- src/main/java/com/provit/domain/schedule/controller/ScheduleController.java
+++ src/main/java/com/provit/domain/schedule/controller/ScheduleController.java
@@ -1,13 +1,12 @@
 package com.provit.domain.schedule.controller;
 
 import com.provit.domain.schedule.dto.ScheduleDto;
+import com.provit.domain.schedule.dto.ScheduleUpdateDto;
 import com.provit.domain.schedule.service.ScheduleService;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/schedule")
@@ -16,8 +15,12 @@ public class ScheduleController {
     private final ScheduleService scheduleService;
 
     @PostMapping("/add")
-    public ResponseEntity<String> addSchedule(@RequestBody ScheduleDto scheduleDto) throws Exception {
-
+    public ResponseEntity<String> addSchedule(@Valid @RequestBody ScheduleDto scheduleDto) throws Exception {
         return scheduleService.addSchedule(scheduleDto);
+    }
+
+    @PutMapping("/update")
+    public ResponseEntity<?> updateSchedule(@RequestBody ScheduleUpdateDto updateDto) throws Exception{
+        return scheduleService.updateSchedule(updateDto);
     }
 }
