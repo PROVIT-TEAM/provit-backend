fbac407bc68b4385dd9b83610fe4a2484dd42bd8
one-zeze <tales0708@gmail.com>
2024-01-17 20:30:28 +0900

    Add method updateSchedule. Implements ScheduleUseCase.

--- src/main/java/com/provit/domain/schedule/service/ScheduleService.java
+++ src/main/java/com/provit/domain/schedule/service/ScheduleService.java
@@ -4,20 +4,24 @@ import com.provit.domain.member.Member;
 import com.provit.domain.member.repository.MemberRepository;
 import com.provit.domain.schedule.Schedule;
 import com.provit.domain.schedule.dto.ScheduleDto;
+import com.provit.domain.schedule.dto.ScheduleUpdateDto;
 import com.provit.domain.schedule.repository.ScheduleRepository;
 import com.provit.global.security.utils.SecurityUtil;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 @Slf4j
 @RequiredArgsConstructor
-public class ScheduleService {
+@Transactional
+public class ScheduleService implements ScheduleUseCase{
     private final ScheduleRepository scheduleRepository;
     private final MemberRepository memberRepository;
 
+    @Override
     public ResponseEntity<String> addSchedule(ScheduleDto scheduleDto) throws Exception {
         Member findMember = memberRepository.findByUsername(SecurityUtil.getLoginUsername())
                 .orElseThrow(() -> new Exception("회원이 없습니다"));
@@ -32,4 +36,26 @@ public class ScheduleService {
 
         return ResponseEntity.ok("등록 완료");
     }
+    @Override
+    public ResponseEntity<String> updateSchedule(ScheduleUpdateDto updateDto) throws Exception {
+        Schedule schedule = scheduleRepository.findById(updateDto.id()).orElseThrow(() -> new Exception("존재하지 않는 일정입니다."));
+        if (updateDto.title().isPresent()){
+            schedule.updateTitle(updateDto.title().get());
+        }
+        if (updateDto.content().isPresent()){
+            schedule.updateContent(updateDto.content().get());
+        }
+
+        return ResponseEntity.ok("수정 완료");
+    }
+//    public ResponseEntity<String> updateTitle(Long id, String title) throws Exception{
+//        Schedule schedule = scheduleRepository.findById(id).orElseThrow(() -> new Exception("존재하지 않는 일정입니다."));
+//        schedule.updateTitle(title);
+//        return ResponseEntity.ok("수정 완료");
+//    }
+//    public ResponseEntity<String> updateContent(Long id, String content) throws Exception{
+//        Schedule schedule = scheduleRepository.findById(id).orElseThrow(() -> new Exception("존재하지 않는 일정입니다."));
+//        schedule.updateContent(content);
+//        return ResponseEntity.ok("수정 완료");
+//    }
 }
