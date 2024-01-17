735172416960a30a883abe1363a3eeb8ee4b8da0
one-zeze <tales0708@gmail.com>
2024-01-17 20:29:04 +0900

    Add method findyById() and findByMember().

--- src/main/java/com/provit/domain/schedule/repository/ScheduleRepository.java
+++ src/main/java/com/provit/domain/schedule/repository/ScheduleRepository.java
@@ -1,8 +1,13 @@
 package com.provit.domain.schedule.repository;
 
+import com.provit.domain.member.Member;
 import com.provit.domain.schedule.Schedule;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+import java.util.Optional;
+
 public interface ScheduleRepository extends JpaRepository<Schedule, Long> {
-    
+    Optional<Schedule> findById(Long id);
+    Optional<List<Schedule>> findByMember(Member member);
 }
